db08266567641986729b66c15acf1f44258bab3f
twitterpontocom <luca.pereira@sptech.school>
2025-03-18 18:51:07 -0300

    Implementado Variação Simulada de 2 sensores a mais de Arduino

--- entregaveisGrupo/arduino/arduino.ino
+++ entregaveisGrupo/arduino/arduino.ino
@@ -1,15 +1,22 @@
+
 // Declaração e atribuição de variaveis
 const int PINO_SENSOR_TEMPERATURA = A0;
-float temperaturaCelsius;
+float sensor1;
+float sensor2;
+float sensor3;
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA); //A0
-  temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01;
-  Serial.print("TempAtual:");  
-  Serial.print(temperaturaCelsius - 22);  
+  sensor1 = (valorLeitura * 5.0 / 1023.0) / 0.01;
+  sensor2 = (sensor1 - 5.6);
+  sensor3 = (sensor1 + 1.4);
+  Serial.print("TempAtual:");
+  Serial.print(sensor3 - 22);  
+  Serial.print(sensor1 - 22);
+  Serial.print(sensor2 - 22);
   Serial.print(" ");  
   Serial.print("TempMáxima:");  
   Serial.print(5);
